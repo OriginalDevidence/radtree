b30a6523565604ea93672a0eb8ebb21eb26e7413
unknown <diego.roymen@gmail.com>
2018-11-03 20:10:03 +0100

    puntos y comas

--- insertar_datos_prueba.sql
+++ insertar_datos_prueba.sql
@@ -8,28 +8,27 @@ INSERT INTO Usuario VALUES
 (NULL, 'JuanEcologico28', 'Juan', 'Pérez Peñasco', '2000-01-01', 'hola@email.com', '$31$1000$AAAAAAAAAAAAAAAAAAAAAGLPiriGp7lXdLflFuOuAyk', 'PARTICIPANTE', 0.0),
 (NULL, 'JuanEcologico28', 'Juan', 'Pérez Peñasco', '2000-01-01', 'hola@email.com', '$31$1000$AAAAAAAAAAAAAAAAAAAAAGLPiriGp7lXdLflFuOuAyk', 'PARTICIPANTE', 0.0),
 (NULL, 'JuanEcologico28', 'Juan', 'Pérez Peñasco', '2000-01-01', 'hola@email.com', '$31$1000$AAAAAAAAAAAAAAAAAAAAAGLPiriGp7lXdLflFuOuAyk', 'PARTICIPANTE', 0.0),
-(NULL, 'JuanEcologico28', 'Juan', 'Pérez Peñasco', '2000-01-01', 'hola@email.com', '$31$1000$AAAAAAAAAAAAAAAAAAAAAGLPiriGp7lXdLflFuOuAyk', 'PARTICIPANTE', 0.0)
+(NULL, 'JuanEcologico28', 'Juan', 'Pérez Peñasco', '2000-01-01', 'hola@email.com', '$31$1000$AAAAAAAAAAAAAAAAAAAAAGLPiriGp7lXdLflFuOuAyk', 'PARTICIPANTE', 0.0);
 
-/* 6 contenidos: 2 noticias, 2 retos, 2 preguntas */
 INSERT INTO Contenido VALUES
 (NULL, 1, 0, '2000-01-01', 'PENDIENTE'),
 (NULL, 2, 0, '2000-01-02', 'PENDIENTE'),
 (NULL, 1, 0, '2000-01-03', 'PENDIENTE'),
 (NULL, 3, 0, '2000-01-04', 'PENDIENTE'),
 (NULL, 4, 0, '2000-01-05', 'PENDIENTE'),
-(NULL, 2, 0, '2000-01-06', 'PENDIENTE')
+(NULL, 2, 0, '2000-01-06', 'PENDIENTE');
 
 INSERT INTO Noticia VALUES
 (1, 'Titulo', 'Cuerpo', 'url'),
-(2, 'Titulo', 'Cuerpo', 'url')
+(2, 'Titulo', 'Cuerpo', 'url');
 
 INSERT INTO Reto VALUES
 (3, 'Titulo', 'Cuerpo'),
-(4, 'Titulo', 'Cuerpo')
+(4, 'Titulo', 'Cuerpo');
 
 INSERT INTO Pregunta VALUES
 (5, 'enunciado'),
-(6, 'enunciado')
+(6, 'enunciado');
 
 /* 5 respuestas para la priemra pregunta y 5 respuestas para la segunda */
 INSERT INTO Respuesta VALUES
@@ -42,7 +41,7 @@ INSERT INTO Respuesta VALUES
 (NULL, 6, 'enunciado', TRUE),
 (NULL, 6, 'enunciado', FALSE),
 (NULL, 6, 'enunciado', FALSE),
-(NULL, 6, 'enunciado', FALSE)
+(NULL, 6, 'enunciado', FALSE);
 
 /* modificar cuerpo solamente, el resto ya esta */
 INSERT INTO Comentario VALUES
@@ -53,7 +52,7 @@ INSERT INTO Comentario VALUES
 (NULL, 3, 1, 'cuerpo', 0, '2000-02-05', NULL),
 (NULL, 1, 2, 'cuerpo', 0, '2000-02-06', NULL),
 (NULL, 1, 2, 'cuerpo', 0, '2000-02-07', NULL),
-(NULL, 2, 2, 'cuerpo', 0, '2000-02-08', 1)
+(NULL, 2, 2, 'cuerpo', 0, '2000-02-08', 1);
 
 /* no hace falta cambiar nada aqui */
 INSERT INTO Contesta VALUES
@@ -76,4 +75,4 @@ INSERT INTO Contesta VALUES
 (2, 7, TRUE),
 (2, 8, TRUE),
 (2, 9, TRUE),
-(2, 10, TRUE)
+(2, 10, TRUE);
