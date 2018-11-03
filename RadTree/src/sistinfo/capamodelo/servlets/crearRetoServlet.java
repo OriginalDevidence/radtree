03b06b471c5059394c601a2b27c1de55beee23d9
Rengentable273 <33006404+Regentable239@users.noreply.github.com>
2018-11-03 16:42:44 +0100

    crearReto.servlet version 1

--- RadTree/src/sistinfo/capamodelo/servlets/crearRetoServlet.java
+++ RadTree/src/sistinfo/capamodelo/servlets/crearRetoServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import sistinfo.capadatos.vo.UsuarioVO;
-import sistinfo.capadatos.dao.UsuarioDAO;
+import sistinfo.capadatos.vo.RetoVO;
+import sistinfo.capadatos.dao.RetoDAO;
 import sistinfo.capadatos.excepciones.AliasYaExistenteException;
 import sistinfo.capadatos.excepciones.EmailYaExistenteException;
 
@@ -24,39 +24,65 @@ public class RegistroServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-    	response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+      /* TODO buscar una forma mejor para hacer esto sin tener que cambiar el encoding todo el rato */
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        Map<String, String> errores = new HashMap<String, String>();
+      	RetoVO reto = extractRetoFromHttpRequest(request, errores);
+
+        if (reto != null) {
+	        	try {
+	        		RetoDAO retoDAO = new RetoDAO();
+	                RetoDAO.insertReto(reto);
+
+	                response.sendRedirect("perfil.jsp"); // DUDA
+
+	            } catch (Exception e) {
+	                response.sendRedirect("errorInterno.html");
+	            }
+	        } else {
+	            request.setAttribute("errores", errores);
+	        }
+    }
+
+    /**
+     * Obtiene los datos de un usuario dado un HttpServletRequest y escribe los errores en errors
+     * @param request
+     * @param errors
+     * @return El usuario si se ha extraido correctamente, o null
+     */
+  public RetoVO extractRetoFromHttpRequest(HttpServletRequest request, Map<String, String> errors) {
 
         //Como obtener la idAutor mirar DUDA
         long idAutor = new long(1);
-        Date fechaRealizacion = new Date(37288713); // TODO
+        Date fechaRealizacion = null; // TODO
 
         Estado estado = new Estado(PENDIENTE);
         long numVisitas = new long(0);
         String titulo = request.getParameter("titulo");
         String cuerpo = request.getParameter("cuerpo");
 
-        if (clave.equals(reclave)) {
-            // TODO hash de la contraseña
-            // DUDA :
-            NoticiaVO reto = new RetoVO(idAutor,numVisitas,fechaRealizacion,estado,titulo,cuerpo);
-            try {
-                NoticiaDAO facade = new RetoDAO();
-                facade.insertUsuario(reto);
-                /* TODO pasar datos del perfil */
-
-                HttpSession session = request.getSession();
-                session.setAttribute("profile", alias);
-                response.sendRedirect("perfil.jsp");
-            } catch (AliasYaExistenteException | EmailYaExistenteException e) {
-            	/* TODO avisar de alias o email ya existente */
-                response.sendRedirect("42_crearReto.html");
-            } catch (Exception e) {
-                response.sendRedirect("paginaError.html");
-            }
-        } else {
-        	/* TODO avisar de que las contraseñas no son iguales */
-            response.sendRedirect("42_crearReto.html");
+        boolean datosCorrectos = true;
+        if (titulo == null || titulo.trim().isEmpty()) {
+          datosCorrectos = false;
+          errors.put("titulo", "Titulo inválido");
         }
-    }
+        if (cuerpo == null || cuerpo.trim().isEmpty()) {
+          datosCorrectos = false;
+          errors.put("cuerpo", "Cuerpo del reto inválido");
+        }
 
+        if (datosCorrectos) {
+          if (clave.equals(reclave)) {
+            String claveHash = PBKDF2Hash.hash(clave.toCharArray());
+            if (claveHash != null) {
+              return new RetoVO(idAutor, numVisitas, fechaRealizacion, estado, titulo, cuerpo);
+            }
+          } else {
+            errors.put("reclave", "La clave no coincide");
+          }
+        }
+        return null;
+  }
 }
