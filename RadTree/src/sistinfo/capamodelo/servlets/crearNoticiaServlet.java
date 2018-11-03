5c43616ba543e6df6709891b78eb22b4f3facf5f
Rengentable273 <33006404+Regentable239@users.noreply.github.com>
2018-11-03 16:29:08 +0100

    crearNoticia.servlet modificado

--- RadTree/src/sistinfo/capamodelo/servlets/crearNoticiaServlet.java
+++ RadTree/src/sistinfo/capamodelo/servlets/crearNoticiaServlet.java
@@ -24,11 +24,40 @@ public class RegistroServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-    	response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+      /* TODO buscar una forma mejor para hacer esto sin tener que cambiar el encoding todo el rato */
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        Map<String, String> errores = new HashMap<String, String>();
+      	NoticiaVO noticia = extractNoticiaFromHttpRequest(request, errores);
+
+        if (noticia != null) {
+	        	try {
+	        		NoticiaDAO noticiaDAO = new NoticiaDAO();
+	                NoticiaDAO.insertNoticia(noticia, null);
+
+	                response.sendRedirect("perfil.jsp");
+	                request.setAttribute("usuario", noticia);
+
+	            } catch (Exception e) {
+	                response.sendRedirect("errorInterno.html");
+	            }
+	        } else {
+	            request.setAttribute("errores", errores);
+	        }
+    }
+
+    /**
+     * Obtiene los datos de un usuario dado un HttpServletRequest y escribe los errores en errors
+     * @param request
+     * @param errors
+     * @return El usuario si se ha extraido correctamente, o null
+     */
+  public NoticiaVO extractNoticiaFromHttpRequest(HttpServletRequest request, Map<String, String> errors) {
 
         //Como obtener la idAutor mirar DUDA
         long idAutor = new long(1);
-        Date fechaRealizacion = new Date(37288713); // TODO
+        Date fechaRealizacion = null; // TODO
 
         Estado estado = new Estado(PENDIENTE);
         long numVisitas = new long(0);
@@ -36,28 +65,41 @@ public class RegistroServlet extends HttpServlet {
         String cuerpo = request.getParameter("cuerpo");
         String url = request.getParameter("url");
 
-        if (clave.equals(reclave)) {
-            // TODO hash de la contraseña
-            // DUDA :
-            NoticiaVO noticia = new NoticiaVO(idAutor,numVisitas,fechaRealizacion,estado,titulo,cuerpo,url);
-            try {
-                NoticiaDAO facade = new NoticiaDAO();
-                facade.insertUsuario(noticia);
-                /* TODO pasar datos del perfil */
-
-                HttpSession session = request.getSession();
-                session.setAttribute("profile", alias);
-                response.sendRedirect("perfil.jsp");
-            } catch (AliasYaExistenteException | EmailYaExistenteException e) {
-            	/* TODO avisar de alias o email ya existente */
-                response.sendRedirect("40_crearNoticia.html");
-            } catch (Exception e) {
-                response.sendRedirect("paginaError.html");
-            }
-        } else {
-        	/* TODO avisar de que las contraseñas no son iguales */
-            response.sendRedirect("40_crearNoticia.html");
+        boolean datosCorrectos = true;
+        if (titulo == null || titulo.trim().isEmpty()) {
+          datosCorrectos = false;
+          errors.put("titulo", "Titulo inválido");
         }
-    }
+        if (cuerpo == null || cuerpo.trim().isEmpty()) {
+          datosCorrectos = false;
+          errors.put("cuerpo", "Cuerpo noticia inválido");
+        }
+
+        if (url == null || alias.url().isEmpty() || !validarURLFormato(url)) {
+           datosCorrectos = false;
+           errors.put("url", "Campo obligatorio");
+           errorsArriba.add("Una url debe ser: http://www.example.com ");
+         } else if (!validarURLFormato(url)) {
+           datosCorrectos = false;
+           errors.put("url", "Formato incorrecto");
+           errorsArriba.add("Una url debe de poser el formato: http://www.example.com ");
+         }
+
+        if (datosCorrectos) {
+          if (clave.equals(reclave)) {
+            String claveHash = PBKDF2Hash.hash(clave.toCharArray());
+            if (claveHash != null) {
+              return new NoticiaVO(idAutor, numVisitas, fechaRealizacion, estado, titulo, cuerpo, url);
+            }
+          } else {
+            errors.put("reclave", "La clave no coincide");
+          }
+        }
+        return null;
+  }
+
+  private boolean validarURLFormato(String url) {
+    return url.matches("^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$");
+  }
 
 }
