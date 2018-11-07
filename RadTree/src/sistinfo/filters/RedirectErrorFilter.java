fe5a66acecebaf0dcb33aabc3c37dc129b3debba
diegoroyo <diego.roymen@gmail.com>
2018-11-07 20:41:07 +0100

    Redirect error (general, para jsp y .do)

--- RadTree/src/sistinfo/filters/RedirectErrorFilter.java
+++ RadTree/src/sistinfo/filters/RedirectErrorFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-public class RedirectJSPFilter implements Filter {
+public class RedirectErrorFilter implements Filter {
 
 	FilterConfig filterConfig = null;
 
